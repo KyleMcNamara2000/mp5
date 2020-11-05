2e111a6e5362a480c82b7ad43b670208c4648c47
kylem <kylemcnamara2000@gmail.com>
2020-11-04 22:49:01 -0600

    heyo

--- classify.py
+++ classify.py
@@ -34,39 +34,96 @@ return - a list containing predicted labels for dev_set
 import numpy as np
 
 def trainPerceptron(train_set, train_labels, learning_rate, max_iter):
-    # TODO: Write your code here
-    # return the trained weight and bias parameters
-    rows, cols = (len(train_set), 32 * 32 * 3)
-    #oldW = [[0 for i in range(cols)] for j in range(rows)]
-    oldW = [0] * 32 * 32 * 3
-    b = 0 #TODO: is this multiple values??? + how do I do
-    for k in range(max_iter):
+    W = np.array([0.0] * 32 * 32 * 3)
+    #W = [0] * 32 * 32 * 3
+    b = 0
+    for ja in range(max_iter):
         for i in range(len(train_set)):
-            for j in range(len(train_set[i])):
-                #oldW[i][j], train_set[i][j]
-                x = train_set[i][j]
-                wi = oldW[j]
-                y_correct = train_labels[i]
-                y_pred = x * wi
-                if y_pred != y_correct:
-                    oldW[j] = wi + learning_rate * (y_correct - y_pred) * x
+            dot = np.sign(np.dot(W, train_set[i]))
+            s = 1
+            if train_labels[i] is np.bool_(False):
+                s = -1
+            if dot != s:
+                k = learning_rate * s
+                dodo = np.dot(k, train_set[i])
+                W += dodo
+                b += learning_rate
+                #print(b)
+
+
+
+    return W, b
+
+
+    '''
+    oldW = np.array([0] * 32 * 32 * 3)
+    #oldW = [0] * 32 * 32 * 3
+    b = 0 #b = w0 * x0, x0 = 1
+    for i in range(len(train_set)):
+        for j in range(len(train_set[i])):
+            #oldW[i][j], train_set[i][j]
+            x = train_set[i][j]
+            wi = oldW[j]
+            y_correct = train_labels[i]
+            y_pred = x * wi
+            if y_pred != y_correct:
+                oldW[j] = wi + learning_rate * (y_correct - y_pred) * x
+                b -= learning_rate
+            else:
+                b += learning_rate
     return oldW, b
+    '''
 
 def classifyPerceptron(train_set, train_labels, dev_set, learning_rate, max_iter):
     # TODO: Write your code here
     # Train perceptron model and return predicted labels of development set
-    #print("ho")
     W, b = trainPerceptron(train_set, train_labels, learning_rate, max_iter)
-    #print(W)
     guesses = []
+    #dot product W .
     for i in range(len(dev_set)):
-        sum = 0
-        for j in range(len(dev_set[i])):
-            sum += W[j] * dev_set[i][j]
+        sum = np.dot(W, dev_set[i])
         sum += b
         guesses.append(np.sign(sum))
     return guesses
 
+
+
+
+def correctLabel(L):
+    negs = 0
+    pos = 0
+    for x in L:
+        if x[1] == 1:
+            pos += 1
+        else:
+            negs += 1
+    if pos > negs:
+        return 1
+    else:
+        return -1
+
 def classifyKNN(train_set, train_labels, dev_set, k):
     # TODO: Write your code here
     return []
+    '''
+    out = []
+    for i in range(len(dev_set)):
+        nearest = [] #nearest pairs (distance, trained value)
+        for j in range(len(train_set)):
+            curr_dist = np.linalg.norm(dev_set[i], zip(train_set[j])[0])
+            '''
+            for m in range(32 * 32 * 3):
+                curr_dist += np.abs(dev_set[i][m] - train_set[j][m])
+            '''
+
+            if j < k:
+                nearest.append((curr_dist, train_labels[j]))
+                nearest.sort()
+            elif curr_dist < nearest[-1][0]:
+                nearest.pop(-1)
+                nearest.append((curr_dist, train_labels[j]))
+                nearest.sort()
+        out.append(correctLabel(nearest))
+
+    return out
+    '''
